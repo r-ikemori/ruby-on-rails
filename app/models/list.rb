f7fee92d1fcf9a3a7f12289ac2d36677b4081cd3
r-ikemori <104720499+r-ikemori@users.noreply.github.com>
2022-05-10 13:05:03 +0000

    [finish]4-11

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,4 +4,8 @@ class List < ApplicationRecord
   validates:title, presence: true
   validates:body, presence: true
   validates:image, presence: true
+  
+  #コメントアウトする
+  # validates:image,presence:true
+  
 end
